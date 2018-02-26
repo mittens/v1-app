fed7ed025907cd350f300fcf0da842e8410aaa67
Ali Zahid <ali.zahid@live.com>
2018-02-26 18:29:10 +0400

    Refactoring

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { AppRegistry } from 'react-native'
 import { Provider } from 'react-redux'
 
 import App from './app/index'
 import store from './app/store'
 
-const GitHub = () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-)
+class GitHub extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+  }
+}
 
 AppRegistry.registerComponent('GitHub', () => GitHub)
